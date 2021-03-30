43f96cd92b87414b0a3b183a2fc12ee419f5c84b
abdulnad <abdullah7998@gmail.com>
2021-03-30 16:41:02 -0400

    UPDATED

--- src/Controllers/GameSelectController.java
+++ src/Controllers/GameSelectController.java
@@ -1,11 +1,13 @@
 package Controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
@@ -14,11 +16,15 @@ import java.util.ResourceBundle;
 
 public class GameSelectController implements Initializable {
 
+    @FXML
+    ImageView sudokuImage, sudokuImage2;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
     }
 
+    /*Sudoku Methods*/
     public void playSudoku(ActionEvent actionEvent) throws Exception {
         Parent page = FXMLLoader.load(getClass().getResource("/Views/sudokuboard.fxml"));
         Scene scene = new Scene(page, 900, 600);
@@ -27,12 +33,22 @@ public class GameSelectController implements Initializable {
         stage.show();
     }
 
-
     public void showImage(MouseEvent mouseEvent) {
+        sudokuImage.setVisible(true);
+        sudokuImage2.setVisible(true);
     }
 
     public void hideImage(MouseEvent mouseEvent) {
+        sudokuImage.setVisible(false);
+        sudokuImage2.setVisible(false);
     }
 
-
+    /*Hangman methods*/
+    public void playHangman(ActionEvent actionEvent) throws Exception {
+        Parent page = FXMLLoader.load(getClass().getResource("/Views/hangman.fxml"));
+        Scene scene = new Scene(page, 900, 600);
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
 }
--- src/Controllers/HangmanController.java
+++ src/Controllers/HangmanController.java
@@ -1,7 +1,17 @@
 package Controllers;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 
+import java.awt.*;
 import java.net.URL;
 import java.util.Collections;
 import java.util.ResourceBundle;
@@ -9,13 +19,26 @@ import java.util.Stack;
 
 public class HangmanController implements Initializable {
 
+    @FXML
+    AnchorPane hangmanAnchorPane;
 
     String[] words = {"School", "Laundry", "House", "Gameboy", "Amazing", "Educational", "Puzzle"};
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        /*Select a rand word from the array*/
         int pick = randomGenerator(words.length - 1);
-        String word = words[pick];
+        String word = words[pick]; //the word
+
+        int pos = 0;
+        for (int i = 0; i < word.length(); i++) {
+            myLabel label = new myLabel(word.charAt(i));
+            label.setFont(new Font("System", 30));
+            hangmanAnchorPane.getChildren().add(label);
+            label.relocate(200 + pos, 200);
+            pos += 45;
+        }
+
     }
 
 
@@ -24,6 +47,13 @@ public class HangmanController implements Initializable {
     }
 
 
+    public void doSomething(MouseEvent mouseEvent) {
+        Text source = (Text) mouseEvent.getSource();
+        System.out.println(source.getText());
+        source.setDisable(true);
+        Paint p = Color.GREY;
+        source.setFill(p);
+    }
 }
 
 
