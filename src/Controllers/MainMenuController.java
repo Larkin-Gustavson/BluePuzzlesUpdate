8071cb4e2626f82add813dbabcce381d2cf63a68
Larkin Gustavson <gustlk@farmingdale.edu>
2021-03-29 20:14:32 -0400

    Added a quit button.

--- src/Controllers/MainMenuController.java
+++ src/Controllers/MainMenuController.java
@@ -1,5 +1,6 @@
 package Controllers;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -52,5 +53,10 @@ public class MainMenuController implements Initializable {
     public void hideImage(MouseEvent mouseEvent) {
         leftImage.setVisible(false);
         rightImage.setVisible(false);
+    }
+
+    @FXML
+    void closeApp(ActionEvent event) {
+        Platform.exit();
     }
 }
