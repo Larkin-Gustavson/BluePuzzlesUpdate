366b34d41ad71e14fa5dcd071bac530e90ea32a3
abdulnad <abdullah7998@gmail.com>
2021-03-30 19:16:16 -0400

    Finished Hangman excepty difficulty and hints.

--- src/Controllers/HangmanController.java
+++ src/Controllers/HangmanController.java
@@ -3,14 +3,20 @@ package Controllers;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 import java.awt.*;
 import java.net.URL;
@@ -23,25 +29,34 @@ public class HangmanController implements Initializable {
 
     @FXML
     AnchorPane hangmanAnchorPane;
+    @FXML
+    Pane hangmanBody, guessWordPane, wordPane;
+    @FXML
+    TextField guessWordField;
+    @FXML
+    Text livesText;
 
-    String[] words = {"School", "Laundry", "House", "Gameboy", "Amazing", "Educational", "Puzzle"};
+    int lives = 6;
+    String[] words = {"School", "Laundry", "House", "Gameboy Advanced", "Amazing", "Educational", "Puzzle",
+            "Blue Puzzles", "Smoke", "Maple Syrup"};
+    String word;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         /*Select a rand word from the array*/
         int pick = randomGenerator(words.length - 1);
-        String word = words[pick].toUpperCase(); //the word
+        word = words[pick].toUpperCase(); //the word
         System.out.println(word);
 
+        /*Positions the word*/
         int pos = 0;
         for (int i = 0; i < word.length(); i++) {
             myLabel label = new myLabel(word.charAt(i));
             label.setFont(new Font("System", 30));
-            hangmanAnchorPane.getChildren().add(label);
-            label.relocate(300 + pos, 320);
-            pos += 45;
+            wordPane.getChildren().add(label);
+            label.relocate(10 + pos, 5);
+            pos += 40;
         }
-
     }
 
 
@@ -57,16 +72,50 @@ public class HangmanController implements Initializable {
         source.setDisable(true);
         Paint p = Color.GREY;
         source.setFill(p);
-        char guess = source.getText().charAt(0);
+        char guess = source.getText().charAt(0); //guessed letter
         /*Changes to word*/
-        for (Node node : hangmanAnchorPane.getChildren()) {
+        boolean guessRight = false;
+        for (Node node : wordPane.getChildren()) {
             if (node instanceof myLabel) {
                 myLabel label = (myLabel) node;
                 if (guess == label.getLetter()) {
                     label.showLetter();
+                    guessRight = true;
                 }
             }
         }
+        if (!guessRight) {
+            lives--;
+            livesText.setText(lives + "");
+            for (Node body : hangmanBody.getChildren()) {
+                if (!body.isVisible()) {
+                    body.setVisible(true);
+                    break;
+                }
+            }
+        }
+        if (lives == 0) {
+            System.out.println("You lose!");
+        }
+    } //End of Method
+
+
+    public void guessWord(ActionEvent actionEvent) {
+        guessWordPane.setVisible(true);
+    }
+
+    public void enterGuess(ActionEvent actionEvent) {
+        guessWordPane.setVisible(false);
+        if (guessWordField.getText().toUpperCase().equals(word))
+            System.out.println("You win!");
+    }
+
+    public void goBack(ActionEvent actionEvent) throws Exception {
+        Parent page = FXMLLoader.load(getClass().getResource("/Views/game_select.fxml"));
+        Scene scene = new Scene(page, 900, 600);
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
     }
 }
 
