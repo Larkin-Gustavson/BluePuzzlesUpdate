aa1e34ef50365ba65172568d1fa473fd79987c6a
abdulnad <abdullah7998@gmail.com>
2021-03-30 17:54:10 -0400

    Added a letter box to hangman.fxml, added a word guesser, and logic to correctly guessed letter

--- src/Controllers/HangmanController.java
+++ src/Controllers/HangmanController.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
@@ -14,6 +15,7 @@ import javafx.scene.text.Text;
 import java.awt.*;
 import java.net.URL;
 import java.util.Collections;
+import java.util.Locale;
 import java.util.ResourceBundle;
 import java.util.Stack;
 
@@ -28,14 +30,15 @@ public class HangmanController implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         /*Select a rand word from the array*/
         int pick = randomGenerator(words.length - 1);
-        String word = words[pick]; //the word
+        String word = words[pick].toUpperCase(); //the word
+        System.out.println(word);
 
         int pos = 0;
         for (int i = 0; i < word.length(); i++) {
             myLabel label = new myLabel(word.charAt(i));
             label.setFont(new Font("System", 30));
             hangmanAnchorPane.getChildren().add(label);
-            label.relocate(200 + pos, 200);
+            label.relocate(300 + pos, 320);
             pos += 45;
         }
 
@@ -48,11 +51,22 @@ public class HangmanController implements Initializable {
 
 
     public void doSomething(MouseEvent mouseEvent) {
+        /*Changes to letter clicked*/
         Text source = (Text) mouseEvent.getSource();
         System.out.println(source.getText());
         source.setDisable(true);
         Paint p = Color.GREY;
         source.setFill(p);
+        char guess = source.getText().charAt(0);
+        /*Changes to word*/
+        for (Node node : hangmanAnchorPane.getChildren()) {
+            if (node instanceof myLabel) {
+                myLabel label = (myLabel) node;
+                if (guess == label.getLetter()) {
+                    label.showLetter();
+                }
+            }
+        }
     }
 }
 
