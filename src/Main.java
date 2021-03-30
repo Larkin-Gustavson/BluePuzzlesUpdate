737b01da41c931f13c8f1bccb92d3394465f0915
abdulnad <abdullah7998@gmail.com>
2021-03-29 20:06:33 -0400

    test comment

--- src/Main.java
+++ src/Main.java
@@ -16,6 +16,7 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root, 900, 600));
         primaryStage.show();
         primaryStage.setResizable(false);
+        //My branch comment
     }
 
     public static void main(String[] args) {launch(args); }
