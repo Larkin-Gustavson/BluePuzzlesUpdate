83dcb9358552e4537af09a6e439b5e1990e4d0e5
Larkin Gustavson <gustlk@farmingdale.edu>
2021-04-29 16:27:48 -0400

    Added REGEX for validating password, minor performance improvements, fixed small bugs, formatting issues, and starting working on getting hints based on difficulty for the Hangman game.

--- src/Controllers/LeaderboardsController.java
+++ src/Controllers/LeaderboardsController.java
@@ -1,9 +1,7 @@
 package Controllers;
 
-import DB.Leaderboard;
+import DB.JigsawLeaderboard;
 import DB.Record;
-import DB.TARecord;
-import DB.TimeAttackLeaderboard;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -16,7 +14,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.net.URL;
@@ -27,68 +24,36 @@ import java.util.ResourceBundle;
 public class LeaderboardsController implements Initializable {
 
     @FXML
-    AnchorPane mainAnchor;
-    @FXML
-    TableView<Record> JigsawTable, hangmanTable, tentsTable, memoryTable;
-    @FXML
-    TableView<TARecord> TATable;
+    TableView<Record> jigsawTable, hangmanTable;
     @FXML
     TableColumn<Record, String> jigsawUser, jigsawDifficulty, jigsawTime, hangmanUser, hangmanDifficulty,
-            hangmanTime, tentsUser, tentsDifficulty, tentsTime, memoryUser, memoryDifficulty, memoryTime;
-    @FXML
-    TableColumn<TARecord, String> TAUser, TAPoints;
+            hangmanTime;
 
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
-        /*Jigsaw*/
+        /* Jigsaw */
         jigsawUser.setCellValueFactory(new PropertyValueFactory<Record, String>("userName"));
         jigsawDifficulty.setCellValueFactory(new PropertyValueFactory<Record, String>("difficulty"));
         jigsawTime.setCellValueFactory(new PropertyValueFactory<Record, String>("time"));
-        /*Hangman*/
-        hangmanUser.setCellValueFactory(new PropertyValueFactory<Record, String>("userName"));
-        hangmanDifficulty.setCellValueFactory(new PropertyValueFactory<Record, String>("difficulty"));
-        hangmanTime.setCellValueFactory(new PropertyValueFactory<Record, String>("time"));
-        /*Tents*/
-        tentsUser.setCellValueFactory(new PropertyValueFactory<Record, String>("userName"));
-        tentsDifficulty.setCellValueFactory(new PropertyValueFactory<Record, String>("difficulty"));
-        tentsTime.setCellValueFactory(new PropertyValueFactory<Record, String>("time"));
-        /*Memory*/
-        memoryUser.setCellValueFactory(new PropertyValueFactory<Record, String>("userName"));
-        memoryDifficulty.setCellValueFactory(new PropertyValueFactory<Record, String>("difficulty"));
-        memoryTime.setCellValueFactory(new PropertyValueFactory<Record, String>("time"));
-        /*Time Attack*/
-        TAUser.setCellValueFactory(new PropertyValueFactory<TARecord, String>("userName"));
-        TAPoints.setCellValueFactory(new PropertyValueFactory<TARecord, String>("points"));
+        /* Hangman */
+        jigsawUser.setCellValueFactory(new PropertyValueFactory<Record, String>("userName"));
+        jigsawDifficulty.setCellValueFactory(new PropertyValueFactory<Record, String>("difficulty"));
+        jigsawTime.setCellValueFactory(new PropertyValueFactory<Record, String>("time"));
         try {
-            JigsawTable.setItems(getRecords("JigsawLeaderboard"));
-            hangmanTable.setItems((getRecords("HangmanLeaderboard")));
-            tentsTable.setItems((getRecords("TentsLeaderboard")));
-            memoryTable.setItems(getRecords("MemoryLeaderboard"));
-            TATable.setItems(getTARecords());
+            jigsawTable.setItems(getRecords());
+
         } catch (SQLException e) {
 
         }
     }
 
-    public ObservableList<Record> getRecords(String game) throws SQLException {
+    public ObservableList<Record> getRecords() throws SQLException {
         ObservableList<Record> records = FXCollections.observableArrayList();
-        ArrayList<String> record = Leaderboard.getAllRecords(game);
-        for (int i = 0; i < Leaderboard.getAllRecords(game).size(); i += 3) {
-            Record jr = new Record(record.get(i), record.get(i + 1), record.get(i + 2));
-            records.add(jr);
-        }
-
-
-        return records;
-    }
-
-    public ObservableList<TARecord> getTARecords() throws SQLException {
-        ObservableList<TARecord> records = FXCollections.observableArrayList();
-        ArrayList<String> record = TimeAttackLeaderboard.getAllRecords();
-        for (int i = 0; i < TimeAttackLeaderboard.getAllRecords().size(); i += 2) {
-            TARecord jr = new TARecord(record.get(i), Integer.parseInt(record.get(i + 1)));
+        ArrayList<String> record = JigsawLeaderboard.getAllRecords();
+        Record jr = null;
+        for (int i = 0; i < JigsawLeaderboard.getAllRecords().size(); i += 3) {
+            jr  = new Record(record.get(i), record.get(i + 1), record.get(i + 2));
             records.add(jr);
         }
 
