6b9e2561033e1fe157c09d5b3d5e5e51de7f12f0
Larkin-Gustavson <larkingustavson@gmail.com>
2022-09-16 15:45:31 -0400

    Updated the picture used for the app (the logo used in the top left of the program).

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/Views/login.fxml")); // load the login area
-        primaryStage.getIcons().add(new Image("https://image.flaticon.com/icons/png/512/417/417752.png")); // get this favicon and show it for the entire app
+        primaryStage.getIcons().add(new Image("https://www.psdgraphics.com/file/puzzle-piece.jpg")); // get this favicon and show it for the entire app
         primaryStage.setTitle("Blue Puzzles"); // set the title of the window of the game
         primaryStage.setScene(new Scene(root, 900, 600)); // the dimensions of the window
         primaryStage.setResizable(false); // make it so that users cannot resize the screen of the game
